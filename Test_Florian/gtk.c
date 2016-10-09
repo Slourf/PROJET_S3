2122e0153d1a0f8bcdcedd804636dc740b132068
Florian Chatelus <florian.chalus@epita.fr>
2016-10-09 19:53:35 +0200

    Ajout de deux bouton (les deux quittent la fenetre) et d'un texte au milieu

--- Test_Florian/gtk.c
+++ Test_Florian/gtk.c
@@ -15,8 +15,8 @@ int main(int argc, char **argv)
 {
 	GtkWidget *MainWindow = NULL;
 	GtkWidget *Label = NULL;
-    GtkWidget *pVBox;
-    GtkWidget *Button;
+    GtkWidget *Table;                   //GtkWidget *pVBox;
+    GtkWidget *Button[4];
 	gtk_init(&argc, &argv);
 
     /*Création de la fenetre*/
@@ -29,29 +29,42 @@ int main(int argc, char **argv)
 	gtk_window_set_title(GTK_WINDOW(MainWindow), "IRIS by Eye for an eye");
 	gtk_window_set_position(GTK_WINDOW(MainWindow), GTK_WIN_POS_CENTER);
     
-    /*Création de la box*/
+
+    
+    /*Création de la box
 
     pVBox = gtk_vbox_new(TRUE, 0);
     gtk_container_add(GTK_CONTAINER(MainWindow), pVBox);
     
+    */
+
+    /*Création de la table*/
+
+    Table = gtk_table_new(3, 2, TRUE);
+    gtk_container_add(GTK_CONTAINER(MainWindow), GTK_WIDGET(Table));
     /*Création du Bouton*/
+    //Bouton 1
 
-    Button = gtk_button_new_with_label("Bouton 1");
-    g_signal_connect(G_OBJECT(Button), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    Button[0] = gtk_button_new_with_label("Quitter");
+    g_signal_connect(G_OBJECT(Button[0]), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    
+    //Bouton 2
 
-    gtk_box_pack_start(GTK_BOX(pVBox), Button, TRUE, FALSE, 0);
+    Button[1] = gtk_button_new_with_label("Bonjour");
+    g_signal_connect(G_OBJECT(Button[1]), "clicked", G_CALLBACK(gtk_main_quit), NULL);
 
-	/*Label = gtk_label_new(NULL);
+    //gtk_box_pack_start(GTK_BOX(pVBox), Button, TRUE, FALSE, 0);
+    gtk_table_attach(GTK_TABLE(Table), Button[0], 0, 1, 2, 3, GTK_EXPAND, GTK_EXPAND, 0, 0);
+    gtk_table_attach(GTK_TABLE(Table), Button[1], 1, 2, 2, 3, GTK_EXPAND, GTK_EXPAND, 0, 0);
+	Label = gtk_label_new(NULL);
 	gtk_label_set_markup(GTK_LABEL(Label), "<b><big> Bonjour !</big></b>");
+    gtk_table_attach(GTK_TABLE(Table), Label, 0, 2, 0, 2, GTK_EXPAND, GTK_EXPAND, 0, 0);
 
-	AddBtn(MainWindow);
-
-	gtk_container_add(GTK_CONTAINER(MainWindow), Label);
-	
+	/*AddBtn(MainWindow);
 
 	g_signal_connect(G_OBJECT(MainWindow), "delete-event",
 					G_CALLBACK(gtk_main_quit), NULL);
-*/
+    */
 	//Ne rien mettre après ça !
 	
 	gtk_widget_show_all(MainWindow);
@@ -61,6 +74,12 @@ int main(int argc, char **argv)
 	return EXIT_SUCCESS;
 }
 
+
+void add_label() {
+        GtkWidget *Label;
+        Label = gtk_label_new(NULL);
+        gtk_label_set_markup(GTK_LABEL(Label), "<b><big> Bonjour ! Comment ca va ?</big></b>");
+}
 
 void AddBtn(GtkWidget *window) {
 	GtkWidget *Button;
