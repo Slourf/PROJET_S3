9b6632a805a50e7ccee21f585b263ef5f3c04dae
Antonio Ren <antonio.ren@epita.fr>
2016-10-13 14:16:53 +0200

    Antonio creation de fonction image noir et blanc et correction du niveau de gris

--- working_features/Greyscale/main.c
+++ working_features/Greyscale/main.c
@@ -73,31 +73,52 @@ SDL_Surface* display_image(SDL_Surface *img) {
   return screen;
 }
 
+SDL_Surface* BlackandWhite(SDL_Surface *imggrey){
+  Uint8 mid = 127;
+  Uint8 blanc = 255;
+  Uint8 noir = 0;
+  for (int x = 0; x < imggrey->w; ++x) {
+        for (int y = 0; y < imggrey->h; ++y) {
+                Uint8 r, g, b;
+                Uint32 pixel = getpixel(imggrey, x, y);
+                SDL_GetRGB(pixel, imggrey->format, &r, &g, &b);
+		if(r>mid){
+			 pixel = SDL_MapRGB(imggrey->format, blanc, blanc,blanc);
 
+		}
+		else{
+			pixel = SDL_MapRGB(imggrey->format, noir,noir, noir);
+		}
+                putpixel(imggrey, x, y, pixel); 
+        }       
+    }
+  return imggrey;
+}
 
 int main() {
-	init_sdl();
+    init_sdl();
     char *path = malloc(256);
     printf("Veuillez entrer le chemin de votre image : ");
     scanf("%256s", path);
-	SDL_Surface *img = load_image(path);
+    SDL_Surface *img = load_image(path);
     free(path);
-	display_image(img);
-	
-	for (int x = 0; x < img->w; ++x) {
-		for (int y = 0; y < img->h; ++y) {
-			Uint8 r, g, b;
-			Uint32 pixel = getpixel(img, x, y);
-			SDL_GetRGB(pixel, img->format, &r, &g, &b);
-			r *= 0.3f;
-			g *= 0.59f;
-			b *= 0.11f;
-			float lumi = (r + g + b) / 3;
-			pixel = SDL_MapRGB(img->format, lumi, lumi, lumi);
-			putpixel(img, x, y, pixel);
-		}
+    display_image(img);	
+    for (int x = 0; x < img->w; ++x) {
+	for (int y = 0; y < img->h; ++y) {
+		Uint8 r, g, b;
+		Uint32 pixel = getpixel(img, x, y);
+		SDL_GetRGB(pixel, img->format, &r, &g, &b);
+		r *= 0.3f;
+		g *= 0.59f;
+		b *= 0.11f;
+		float lumi = (r + g + b);
+		pixel = SDL_MapRGB(img->format, lumi, lumi, lumi);
+		putpixel(img, x, y, pixel);
 	}
-	display_image(img);
-	free(img);
-	return 0;
+    }
+    display_image(img);
+    img= BlackandWhite(img);
+    display_image(img);
+    free(img);
+    return 0;
 }
