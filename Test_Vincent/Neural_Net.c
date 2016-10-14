38e4f27fee9edd3558b84bff1177ab5ee622e3f0
Vincent Trinh <vincent.trinh@epita.fr>
2016-10-14 17:24:33 +0200

    Neural Network functions begining

--- Test_Vincent/Neural_Net.c
+++ Test_Vincent/Neural_Net.c
@@ -2,8 +2,12 @@
 # include <stdio.h>
 # include <err.h>
 # include <assert.h>
+# include <math.h>
+/*
 
+ONLY WORKS FOR XOR FUNCTION
 
+*/
 void readFile(char* path)
 {
 	FILE* file = fopen(path, "r");
@@ -24,7 +28,6 @@ void readFile(char* path)
 	}
 }
 
-
 void writeFile(char* path, char* str) 
 {
 	FILE* file = fopen(path, "w");
@@ -41,11 +44,60 @@ void writeFile(char* path, char* str)
 	}
 }
 
+int xorFunction(int x1, int x2)
+{
+
+	return 0 ;
+}
+
+float sigmoid(double f)
+{
+	return (float)(1/(1+exp(f)));
+}
+
+float sum(int nbinput, int *inputs, int nbweight, int *weight)
+{
+	float res = 0;
+	int *endinput = inputs + nbinput;
+	int *endweight = weight + nbweight;
+	for (;inputs < endinput - 1 ; ++inputs )
+	{
+		res+= (*inputs) * (*weight);
+		++weight;
+	}
+	res = sigmoid(res - *weight);
+	return res;
+}
+
+
+
+int activate(float sig)
+{
+	return 1 ? sig>=1/2 : 0;
+}
+
+void init(char *path, float *coeff)
+{
+	char str[1000];
+	for (int i = 0 ; i < 3 ; ++i)
+	{
+		*coeff = ((float)rand() / (double)RAND_MAX) * 0.2 - 0.1;
+		coeff++;
+	}
+	coeff= coeff-3; 
+	for (int j = 0; j < 3; ++j)
+		{
+			
+		}
+
+	writeFile( path, coeff );
+}
 
 int main(int argc,char *argv[])
 {
-	
+		
 	writeFile("test.txt", argv[1]);
 	readFile("test.txt");
 	return 0;
+
 }
